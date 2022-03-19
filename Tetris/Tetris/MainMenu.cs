28341ee685018634df0077de73f1a30a886a6d41
Sarah Rodrigues <sarahrfs91@gmail.com>
2022-03-19 05:47:53 -0300

    Tela Inicial

---
+++ Tetris/Tetris/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Tetris
+{
+    class MainMenu
+    {
+        public static void writeTitle()
+        {
+            string title = @"
+            
+ _______  _______  _______  ______    ___   _______ 
+|       ||       ||       ||    _ |  |   | |       |
+|_     _||    ___||_     _||   | ||  |   | |  _____|
+  |   |  |   |___   |   |  |   |_||_ |   | | |_____ 
+  |   |  |    ___|  |   |  |    __  ||   | |_____  |
+  |   |  |   |___   |   |  |   |  | ||   |  _____| |
+  |___|  |_______|  |___|  |___|  |_||___| |_______|
+
+
+";
+
+            Console.WriteLine(title, System.Drawing.Color.Gold);
+        }
+    }
+}
--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,16 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            MainMenu.writeTitle();
+            
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Aperte Enter para começar");
+            Console.WriteLine("As setas movem para a direita e para a esquerda");
+            Console.WriteLine("A tecla espaço rotaciona a peça");
+            Console.WriteLine("Aperte esc para sair");
+            Console.ReadKey(true);
         }
     }
 }
